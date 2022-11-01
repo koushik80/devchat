6999826536b09bd2dbfb90b83fd94c79d5f4ee52
koushik <koushik.dey@live.com>
2022-11-02 01:56:14 +0200

    router, controller,views

---
+++ router/usersRouter.js
@@ -0,0 +1,13 @@
+// external imports
+const express = require("express");
+
+// internal imports
+
+const { getUsers } = require("../controller/usersController");
+
+const router = express.Router();
+
+// users page
+router.get("/", getUsers);
+
+module.exports = router;

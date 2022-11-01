ee867091ad951e3d7bf7dd3a3d4a94894cbc6b4b
koushik <koushik.dey@live.com>
2022-11-01 14:35:59 +0200

    db setup with mongoose

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
+dotenv.config();
+
+// DB connection
+
+mongoose
+  .connect(process.env.CONNECT_MONGO, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("db connected successfully!"))
+  .catch((err) => console.log(err));

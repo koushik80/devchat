48a2b84b5bb95214d56b2ed0e3b4d3e2e25b69b1
koushik <koushik.dey@live.com>
2022-11-01 16:47:09 +0200

    middlewares

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 dotenv.config();
@@ -8,9 +10,30 @@ dotenv.config();
 // DB connection
 
 mongoose
-  .connect(process.env.CONNECT_MONGO, {
+  .connect(process.env.MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => console.log("db connected successfully!"))
   .catch((err) => console.log(err));
+
+// request parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// set view engine
+app.set("view engine", "ejs");
+
+// set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// parse cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// routing setup
+
+//error handling
+
+app.listen(process.env.PORT, () => {
+  console.log(`app listening to port ${process.env.PORT}`);
+});

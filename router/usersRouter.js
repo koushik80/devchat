1e74b67bc3d6be6d7ca0a553e14cc9912c2610aa
koushik <koushik.dey@live.com>
2022-11-02 13:00:53 +0200

    updated router

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,12 +2,12 @@
 const express = require("express");
 
 // internal imports
-
 const { getUsers } = require("../controller/usersController");
+const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
 
 const router = express.Router();
 
 // users page
-router.get("/", getUsers);
+router.get("/", decorateHtmlResponse("Users"), getUsers);
 
 module.exports = router;

cc617f1cb00d636f9977b245ad9adfa294f6bf4a
koushik <koushik.dey@live.com>
2022-11-04 19:24:15 +0200

    login checker

--- router/inboxRouter.js
+++ router/inboxRouter.js
@@ -1,13 +1,15 @@
 // external imports
+// external imports
 const express = require("express");
 
 // internal imports
 const { getInbox } = require("../controller/inboxController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const { checkLogin } = require("../middlewares/common/checkLogin");
 
 const router = express.Router();
 
 // inbox page
-router.get("/", decorateHtmlResponse("Inbox"), getInbox);
+router.get("/", decorateHtmlResponse("Inbox"), checkLogin, getInbox);
 
 module.exports = router;
